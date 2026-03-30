d257dc32a196a7e82591adf884ca919ce95f3dba
oleh skrypnyk <oleh.skrypnyk@ukr.net>
2026-03-30 19:20:26 +0100

    Update error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,15 +1,15 @@
-import createHttpError from "http-errors";
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof createHttpError.HttpError) {
+  if (err instanceof HttpError) {
     return res.status(err.status).json({
       message: err.message || err.name,
     });
   }
 
-  const isProd = process.env.NODE_ENV === "production";
+  const isProd = process.env.NODE_ENV === 'production';
 
   res.status(500).json({
-    message: isProd ? "Internal Server Error" : err.stack,
+    message: isProd ? 'Something went wrong' : err.stack,
   });
 };
